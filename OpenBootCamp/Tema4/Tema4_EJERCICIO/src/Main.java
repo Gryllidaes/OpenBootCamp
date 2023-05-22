8c472ba7d5870f0524b48bce68737e10edd89563
Gryllidae UBUNTU <acostagrillo@outlook.com>
2023-05-22 13:43:22 +0200

    Tarea 4 Finalizada Completamente

--- OpenBootCamp/Tema4/Tema4_EJERCICIO/src/Main.java
+++ OpenBootCamp/Tema4/Tema4_EJERCICIO/src/Main.java
@@ -23,7 +23,7 @@ public class Main {
 
             while (numeroWhile < 3) {
                 numeroWhile++;
-                System.out.println("El número es numeroWhile es " + numeroWhile);
+                System.out.println("El número es numeroWhile es: " + numeroWhile);
             }
             System.out.println();
         }
@@ -35,14 +35,41 @@ public class Main {
 
             do {
                 numeroDoWhile++;
-                System.out.println("El número es numeroDoWhile es " + numeroDoWhile);
+                System.out.println("El número es numeroDoWhile es: " + numeroDoWhile);
             } while (numeroDoWhile <3) ;
+
+            System.out.println();
         }
 
 
         // Parte 4: For
         {
-            int numeroFor = 0;
+            for (int numeroFor = 0; numeroFor <= 3; numeroFor++)
+                System.out.println("El número for es: " + numeroFor);
+            System.out.println();
         }
+
+
+        // Parte 5: Switch
+        {
+           String estacion = "verano";
+
+           switch (estacion){
+               case "otoño":
+                   System.out.println("Es Otoño");
+                   break;
+               case "Invierno":
+                   System.out.println("Es invierno");
+                   break;
+               case "Primavera":
+                   System.out.println("Es Primavera");
+                   break;
+               default:
+                   System.out.println("Es Verano");
+                   break;
+
+           }
+        }
+
     }
 }
