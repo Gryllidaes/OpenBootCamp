3fe13391bdbe8586b5c36c4805976e643e2ffe1f
Gryllidae UBUNTU <acostagrillo@outlook.com>
2023-05-22 12:51:46 +0200

    Borrado

--- OpenBootCamp/Tema4/Tema4_EJERCICIO/src/Main.java
+++ OpenBootCamp/Tema4/Tema4_EJERCICIO/src/Main.java
@@ -1,5 +1,31 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("TEMA 4 ");
+
+        // Parte 1: If
+        int numeroIf = 5;
+
+        if (numeroIf > 0) {
+            System.out.println("El número es positivo");
+        } else if (numeroIf < 0) {
+            System.out.println("El número es negativo");
+        } else {
+            System.out.println("El número es 0");
+        }
+
+        // Parte 2: While
+        int numeroWhile < 3;
+
+        while (numeroWhile > 0) {
+            System.out.println("El número es positivo");
+        } else if (numeroIf < 0) {
+            System.out.println("El número es negativo");
+        } else {
+            System.out.println("El número es 0");
+        }
+
+
+
+
     }
 }
