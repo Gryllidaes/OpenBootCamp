2b601dea9ddccc54e38b679f091aa8707b3a5310
Gryllidae UBUNTU <acostagrillo@outlook.com>
2023-05-22 13:20:22 +0200

    Tarea 4 Completada

--- OpenBootCamp/Tema4/Tema4_EJERCICIO/src/Main.java
+++ OpenBootCamp/Tema4/Tema4_EJERCICIO/src/Main.java
@@ -1,31 +1,48 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("TEMA 4 ");
+        System.out.println("TEMA 4: \n");
 
         // Parte 1: If
-        int numeroIf = 5;
+        {
+            int numeroIf = 5;
 
-        if (numeroIf > 0) {
-            System.out.println("El número es positivo");
-        } else if (numeroIf < 0) {
-            System.out.println("El número es negativo");
-        } else {
-            System.out.println("El número es 0");
+            if (numeroIf > 0) {
+                System.out.println("El número es positivo\n");
+            } else if (numeroIf < 0) {
+                System.out.println("El número es negativo\n");
+            } else {
+                System.out.println("El número es 0\n");
+            }
         }
+
 
         // Parte 2: While
-        int numeroWhile < 3;
+        {
+            int numeroWhile = 0;
 
-        while (numeroWhile > 0) {
-            System.out.println("El número es positivo");
-        } else if (numeroIf < 0) {
-            System.out.println("El número es negativo");
-        } else {
-            System.out.println("El número es 0");
+
+            while (numeroWhile < 3) {
+                numeroWhile++;
+                System.out.println("El número es numeroWhile es " + numeroWhile);
+            }
+            System.out.println();
         }
 
 
+            // Parte 3: DoWhile
+        {
+            int numeroDoWhile = 0;
+
+            do {
+                numeroDoWhile++;
+                System.out.println("El número es numeroDoWhile es " + numeroDoWhile);
+            } while (numeroDoWhile <3) ;
+        }
 
 
+        // Parte 4: For
+        {
+            int numeroFor = 0;
+        }
     }
 }
