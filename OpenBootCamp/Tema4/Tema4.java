f106c581c2d3c77f13e425d923afede38a710c21
Gryllidae PC <acostagrillo@outlook.com>
2023-05-22 12:09:25 +0200

    Ejercicio Tema 4

---
+++ OpenBootCamp/Tema4/Tema4.java
@@ -0,0 +1,11 @@
+public class Tema4 {
+    int numeroIf = 5;
+
+if (numeroIf > 0) {
+    System.out.println("El número es positivo");
+} else if (numeroIf < 0) {
+    System.out.println("El número es negativo");
+} else {
+    System.out.println("El número es 0");
+}
+}
